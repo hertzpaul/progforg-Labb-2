4ba1937acb35d642a4b12ba077f192c31d93e987
kevinmartiin <nextgenallu@hotmail.com>
2024-03-12 11:30:31 +0100

    Add project files.

---
+++ Time_labb/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Time_labb
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
